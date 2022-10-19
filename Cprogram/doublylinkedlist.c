6bad1057a913cda4174da419926fb093bdc905c8
HELLCASTER145 <92991970+HELLCASTER145@users.noreply.github.com>
2022-10-19 17:36:38 +0530

    Create doublylinkedlist.c

---
+++ Cprogram/doublylinkedlist.c
@@ -0,0 +1,22 @@
+
+void push(Node** head_ref, int new_data)
+{
+    /* 1. allocate node */
+    Node* new_node = new Node();
+ 
+    /* 2. put in the data */
+    new_node->data = new_data;
+ 
+    /* 3. Make next of new node as head
+    and previous as NULL */
+    new_node->next = (*head_ref);
+    new_node->prev = NULL;
+ 
+    /* 4. change prev of head node to new node */
+    if ((*head_ref) != NULL)
+        (*head_ref)->prev = new_node;
+ 
+    /* 5. move the head to point to the new node */
+    (*head_ref) = new_node;
+}
+ 
